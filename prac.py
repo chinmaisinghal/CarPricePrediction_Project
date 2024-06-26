1d73e3561bf63e897db3c055049ea6d43a4e15cc
chinmaisinghal <chinmaisinghal@gmail.com>
2024-06-26 13:36:11 +0530

    My Project

---
+++ prac.py
@@ -0,0 +1,12 @@
+from tkinter import*
+from tkinter import PhotoImage
+root = Tk()
+root.geometry("1080x720")
+root.title("CarWorth")
+w = Label(root, text='Car Price Predictor')
+w.pack()
+root.config(bg="black")
+#bg_image = PhotoImage(file="car background.png")
+
+
+root.mainloop()
